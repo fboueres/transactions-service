ac0e17ded3e6e4b518e627deb7df2b0433aecc05
Fernando Boueres <132353494+fboueres@users.noreply.github.com>
2025-11-09 23:38:28 -0300

    describe TransactionController

---
+++ src/infrastructure/web/transaction_controller.ts
@@ -0,0 +1,42 @@
+import type { Request, Response } from "express";
+import type { TransactionService } from "../../application/services/transaction_service";
+import type { CreateTransactionDTO } from "../../application/dtos/create_transaction_dto";
+
+export class TransactionController {
+    constructor(
+        private transactionService: TransactionService,
+    ) {}
+
+    async createTransaction(req: Request, res: Response): Promise<Response> {
+        const transactionDTO: CreateTransactionDTO = {
+            sendingAccountId: req.body.sending_account_id,
+            receivingAccountId: req.body.receiving_account_id,
+            amount: req.body.amount,
+        };
+
+        const transaction = await this
+            .transactionService.createTransaction(transactionDTO);
+
+        return res
+            .status(201)
+            .json({
+                _links: `/transactions/${transaction.getId()}`,
+                _embedded: {
+                    sendig_account: {
+                        _links: {
+                            self: `/accounts/${transaction.getSendingAccount().getId()}`
+                        },
+                        id: transaction.getSendingAccount().getId(),
+                        name: transaction.getSendingAccount().getName(),
+                    },
+                    receiving_account: {
+                        _links: {
+                            self: `/accounts/${transaction.getReceivingAccount().getId()}`
+                        },
+                        id: transaction.getReceivingAccount().getId(),
+                        name: transaction.getReceivingAccount().getName(),
+                    }
+                }
+            });
+    }
+}
---
+++ src/infrastructure/web/transaction_controller_e2e.test.ts
@@ -0,0 +1,113 @@
+import express from "express";
+import request from "supertest";
+import { DataSource } from "typeorm";
+import { AccountRepository } from "../../domain/repositories/account_repository";
+import { AccountService } from "../../application/services/account_service";
+import { AccountController } from "./account_controller";
+import { TransactionService } from "../../application/services/transaction_service";
+import { TransactionRepository } from "../../domain/repositories/transaction_repository";
+import { AccountEntity } from "../persistence/entities/account_entity";
+import { TransactionEntity } from "../persistence/entities/transaction_entity";
+import { TypeORMAccountRepository } from "../repositories/typeorm_account_repository";
+import { TypeORMTransactionRepository } from "../repositories/typeorm_transaction_repository";
+import { TransactionController } from "../web/transaction_controller";
+
+const app = express();
+
+let dataSource: DataSource;
+let accountController: AccountController;
+let accountService: AccountService;
+let accountRepository: AccountRepository;
+let transactionController: TransactionController;
+let transactionService: TransactionService;
+let transactionRepository: TransactionRepository;
+
+beforeAll(async () => {
+    dataSource = new DataSource({
+        type: "sqlite",
+        database: ":memory:",
+        dropSchema: true,
+        entities: [AccountEntity, TransactionEntity],
+        synchronize: true,
+        logging: false,
+    });
+
+    await dataSource.initialize();
+
+    accountRepository = new TypeORMAccountRepository(
+        dataSource.getRepository(AccountEntity),
+    );
+    accountService = new AccountService(accountRepository);
+    accountController = new AccountController(
+        accountService,
+    );
+
+    transactionRepository = new TypeORMTransactionRepository(
+        dataSource.getRepository(TransactionEntity),
+    );
+    transactionService = new TransactionService(
+        transactionRepository,
+        accountService,
+    );
+    transactionController = new TransactionController(
+        transactionService,
+    );
+
+    app.post("/accounts", (req, res, next) => {
+        accountController.createAccount(req, res).catch((err) => next(err));
+    });
+    app.post("/transactions", (req, res, next) => {
+        transactionController.createTransaction(req, res).catch((err) => next(err));
+    });
+});
+
+afterAll(async () => {
+    await dataSource.destroy();
+});
+
+describe("TransactionController E2E", () => {
+    it("should create a transaction and retrieve it by ID", async () => {
+        const sendingAccount = await request(app)
+            .post("/accounts")
+            .send({
+                name: "Sending Account",
+            });
+
+        const receivingAccount = await request(app)
+            .post("/accounts")
+            .send({
+                name: "Receiving Account",
+            });
+        
+        const res = await request(app)
+            .post("/transactions")
+            .send({
+                sending_account_id: sendingAccount.body.id,
+                receiving_account_id: receivingAccount.body.id,
+                amount: 500.00,
+            });
+
+        expect(res.status).toBe(201);
+        expect(res.body._links.self).not.toBeNull();
+        
+        expect(res.body._embedded.sending_account._links.self)
+            .toBe(`/accounts/${sendingAccount.body.id}`);
+        expect(res.body._embedded.sending_account.id)
+            .toBe(sendingAccount.body.id);
+        expect(res.body._embedded.sending_account.name)
+            .toBe(sendingAccount.body.name);
+        
+        expect(res.body._embedded.receiving_account._links.self)
+            .toBe(`/accounts/${receivingAccount.body.id}`);
+        expect(res.body._embedded.receiving_account.id)
+            .toBe(sendingAccount.body.id);
+        expect(res.body._embedded.receiving_account.name)
+            .toBe(receivingAccount.body.name);
+
+        expect(res.body.sending_account_id)
+            .toBe(sendingAccount.body.id);
+        expect(res.body.receiving_account_id)
+            .toBe(receivingAccount.body.id);
+        expect(res.body.amount).toBe(500);
+    });
+});
